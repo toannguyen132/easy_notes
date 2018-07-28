5caf622b8b4ae40c892ca4aaef92ac231101d022
Toan Nguyen <n.minhtoan@gmail.com>
2018-07-28 10:12:24 -0700

    create account

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'users#new'
+
+  get 'users/new' => 'users#new', as: :new_user
+
+  post 'users' => 'users#create'
+
 end
